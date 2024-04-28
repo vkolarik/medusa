09b9a768b8afa9342511b33ac6efd0a15a0a0167
Vojtech Kolarik <xkolari1@mendelu.cz>
2024-04-28 21:22:58 +0200

    Add variants fetching

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -3,9 +3,11 @@ import { IProductPreview } from "../modules/Product"
 import { getProductsListWithSort, getRegion, retrievePricedProductById } from "@lib/data"
 import { getProductPrice } from "@lib/util/get-product-price"
 import { Region } from "@medusajs/medusa"
+import { PricedProduct } from "@medusajs/medusa/dist/types/pricing"
 
 
 export const MedusaApiLibrary = new Medusa({ baseUrl: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL!!, maxRetries: 3 })
+export const ACTIVE_COUNTRY_CODE : string = "cz"
 
 export const MedusaApi = {
   async getProductPreviews(): Promise<IProductPreview[] | null> {
@@ -21,40 +23,47 @@ export const MedusaApi = {
         region_id: region.id
       },
       sortBy: "created_at",
-      countryCode: "cz",
+      countryCode: ACTIVE_COUNTRY_CODE,
     });
 
     const res: (IProductPreview | null)[] = await Promise.all(
       response.products.map(async (productPreview) => {
-        const pricedProduct = await retrievePricedProductById({
+        const pricedProduct : PricedProduct | null  = await retrievePricedProductById({
           id: productPreview.id,
-          regionId: "reg_01HR5E816NSE3ZAK0DBMZPG5AB",
+          regionId: region.id,
         });
 
         if (!pricedProduct) {
           return null;
         }
 
+        if (!pricedProduct.variants) {
+          return null;
+        }
+
+
         const { cheapestPrice } = getProductPrice({
           product: pricedProduct,
           region: region,
         });
 
-        return {
+        console.log(pricedProduct.variants)
+
+        const r : IProductPreview = {
           id: parseInt(productPreview.id),
           image: productPreview.thumbnail || '',
           title: productPreview.title,
-          route: productPreview.handle ? `/products/${productPreview.handle}` : '',
-          sizes: [], // You might want to define how to get sizes from ProductPreviewType
-          price: cheapestPrice?.calculated_price || "Price not found",
-          colors: [], // You might want to define how to get colors from ProductPreviewType
+          route: productPreview.handle ? `/produkty/${productPreview.handle}` : '',
+          sizes: pricedProduct.variants.map((variant) => variant.title || ""),
+          price: cheapestPrice?.calculated_price.slice(4, -3) || "Nedefinováno",
+          colors: [],
         };
+
+        return r
       })
     );
 
     return res.filter((product) => product !== null) as IProductPreview[];
   }
-
-
 
 }
